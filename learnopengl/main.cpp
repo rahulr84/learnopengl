64a7270401414e5f6c0dd83629c4583c19a07ce3
RRamachandran <rahul.ramachandran@harman.com>
2021-03-21 09:48:08 +0100

    Initialize GLFW and GLAD.

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -5,6 +5,39 @@
 /* Main function */
 int main()
 {
-	
+	/*****************************/
+	/**** SETUP GLFW AND GLAD ****/
+	/*****************************/
+
+	glfwInit(); // Initialize GLFW
+
+	/* Configure GLFW using glfwWindowHint functions */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	/* Create a Window object */
+	GLFWwindow* window = glfwCreateWindow(800, /* Width */
+		600, /* Height */
+		"LearnOpenGL", /* Name of the window */
+		NULL, NULL /* Ignore these arguments */
+	);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the context of our window the main context on the current thread */
+	glfwMakeContextCurrent(window);
+
+	/* Initialize GLAD */
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
 	return 0;
 }
