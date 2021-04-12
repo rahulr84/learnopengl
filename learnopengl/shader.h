28ff2d37270be6de80be0a09ac10bbdc1a0d3b52
Rahul Ramachandran <rahulr.84@gmail.com>
2021-04-12 15:44:32 +0200

    Code to retrieve the vertex/fragment source code from filepath

--- learnopengl/shader.h
+++ learnopengl/shader.h
@@ -15,7 +15,55 @@ public:
 	unsigned int ID;
 
 	// constructor reads and builds the shader
-	Shader(const char* vertexPath, const char* fragmentpath);
+	Shader(const char* vertexPath, const char* fragmentpath) 
+	{
+		/*************************************************************/
+		/* 1. Retrieve the vertex/fragment source code from filepath */
+		/*************************************************************/
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+		std::string vertexCode;
+		std::string fragmentCode;
+
+		// ensure ifstream objects can throw exceptions:
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try
+		{
+			// open files
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentpath);
+
+			// read file's buffer contents into stream
+			std::stringstream vShaderStream, fShaderStream;
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+
+			// close file handlers
+			vShaderFile.close();
+			fShaderFile.close();
+
+			// convert stream to string
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+		}
+		catch(std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+		}
+
+		// convert string to char buffer
+		const char* vShaderCode = vertexCode.c_str();
+		const char* fShaderCode = fragmentCode.c_str();
+
+		/*************************************************************/
+		/* 2. Compile and link shaders                               */
+		/*************************************************************/
+
+
+
+	}
 
 	// use/activate the Shader
 	void use();
