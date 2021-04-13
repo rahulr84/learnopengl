4f6139815256f2e51d35f1ec7d76f45039c86bf3
Rahul Ramachandran <rahulr.84@gmail.com>
2021-04-13 09:17:24 +0200

    Mix vertex color with texture color

--- learnopengl/shader.fs
+++ learnopengl/shader.fs
@@ -13,5 +13,5 @@ void main()
 	//FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 	//FragColor = vertexColor;
 	//FragColor = vec4(ourColor, 1.0f);
-	FragColor = texture(ourTexture, TexCoord);
+	FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0f);
 }
